40af0704e1cc203ad98a2f8888cccf45a7214d2d
Rogovoy Maxim <39128608+Norlet@users.noreply.github.com>
2023-08-16 18:45:37 +0400

    Update csv_sum.py

--- DeepPavlov/Quest 1 (Code Review)/csv_sum.py
+++ DeepPavlov/Quest 1 (Code Review)/csv_sum.py
@@ -2,3 +2,21 @@ import pandas as pd
 
 df = pd.read_csv("data.csv", sep=",", header=None)
 print(df.values.sum()==10)
+
+#добавим проверку на валидность данных
+
+def check_sum(datapath, target_sum):
+    df = pd.read_csv(datapath, sep=",", header=None)
+    try:
+        for col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors='raise')
+    except Exception:
+        print('Dataframe contain not numeric type of data')
+        exit()
+    return df.values.sum() == target_sum
+
+
+if __name__ == "__main__":
+    config = read_yaml('config.yaml')
+    assert os.path.exists(config['DATAPATH']), "Файл не найден"
+    print(check_sum(config['DATAPATH'], config['TARGET_SUM']))
