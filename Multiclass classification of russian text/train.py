cd6c6e3b0f46451bc9373ecae264604f6d08df8e
Rogovoy Maxim <39128608+Norlet@users.noreply.github.com>
2023-12-20 15:11:58 +0400

    Update train.py

--- Multiclass classification of russian text/train.py
+++ Multiclass classification of russian text/train.py
@@ -1,4 +1,15 @@
--------------------------------------------------------------------------------------- # оставить тут функцию DUMMY CLASSIFIER
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score
+from sklearn.dummy import DummyClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+
 def dummy_mod(model, params, features_train, target_train):
     grid = GridSearchCV(estimator=model, param_grid=params, scoring='f1_macro', cv=5)
     grid.fit(features_train_idf, target_train_encoded)
@@ -8,16 +19,7 @@ def dummy_mod(model, params, features_train, target_train):
     best_model.fit(features_train, target_train)
     
     return best_model
--------------------------------------------------------------------------------------- #добавить в ноутбук DUMMY CLASSIFIER 
-dummy_grid = {
-    'strategy': ['most_frequent', 'stratified', 'prior', 'uniform', 'constant'],
-    'constant': ['Вежливость сотрудников магазина', 'Время ожидания у кассы']
-}
 
-dummy_model = DummyClassifier(random_state=42)
-best_dummy_model = dummy_mod(dummy_model, dummy_grid, features_train, target_train) 
-
--------------------------------------------------------------------------------------- #оставить тут функцию LOGISTIC REGRESSION 
 def calculate_f1_score_logreg(true, predicted):
     return f1_score(true, predicted, average='weighted')
 
@@ -31,16 +33,9 @@ def logreg_mod(model, params, features_train_idf, target_train_encoded, features
     best_model.fit(features_train_idf, target_train_encoded)
     predictions = best_model.predict(features_test_idf)
     return best_model, predictions
---------------------------------------------------------------------------------------#добавить в нотубук LOGISTIC REGRESSION
-logreg_model = LogisticRegression(random_state=42, solver='liblinear', max_iter=1000)
-param_grid = {'model__penalty': ['l1', 'l2'], 'model__C': list(range(1, 15, 3))}
-
-best_logreg_model, logreg_predictions = logreg_mod(logreg_model, param_grid, features_train_idf, target_train_encoded, features_test_idf)
 
 
-
---------------------------------------------------------------------------------------#оставить тут функцию RANDOM FOREST
-def calculate_f1_score(true, predicted):
+def calculate_f1_score_rf(true, predicted):
     return f1_score(true, predicted, average='weighted')
 
 # Функция для обучения модели и получения предсказаний
@@ -50,7 +45,7 @@ def rf_model(model, params, features_train_idf, target_train, features_test_idf,
         'max_depth': [3, 5, 7, None]
     }
     
-    grid = GridSearchCV(RandomForestClassifier(random_state=42), param_grid, scoring=make_scorer(calculate_f1_score), cv=5)
+    grid = GridSearchCV(RandomForestClassifier(random_state=42), param_grid, scoring=make_scorer(calculate_f1_score_rf), cv=5)
     grid.fit(features_train_idf, target_train)
     
     print(f'Лучшие параметры: {grid.best_params_}')
@@ -59,23 +54,14 @@ def rf_model(model, params, features_train_idf, target_train, features_test_idf,
     best_model.fit(features_train_idf, target_train)
     predictions = best_model.predict(features_test_idf)
     
-    f1 = calculate_f1_score(target_test, predictions)
+    f1 = calculate_f1_score_rf(target_test, predictions)
     print("F1-score:", f1)
     
     return best_model, predictions
 
 
--------------------------------------------------------------------------------------- # добавить в ноутбук RANDOM FOREST
-
-best_rf_model, rf_predictions = rf_model(features_train_idf, target_train, features_test_idf, target_test)
-
-
-
-
--------------------------------------------------------------------------------------- # оставить тут функцию KNN
-
 # Определение функции для оценки модели и вывода F1-меры
-def calculate_f1_score(true, predicted):
+def calculate_f1_score_knn(true, predicted):
     return f1_score(true, predicted, average='weighted')
 
 # Функция для обучения модели и получения предсказаний
@@ -89,19 +75,8 @@ def knn_model(model, params, features_train_idf, target_train_encoded, features_
     predictions = best_model.predict(features_test_idf)
     return best_model, predictions
 
-
--------------------------------------------------------------------------------------- # добавить в ноутбук KNN
-
-KNN_model = KNeighborsClassifier()
-KNN_params = {'n_neighbors': np.arange(3, 50, 2)}
-
-best_KNN_model, KNN_y = modeling(KNN_model, KNN_params, features_train_idf, target_train_encoded, features_test_idf)
-f1 = calculate_f1_score(target_test_encoded, KNN_y)
-print(f'F1-мера: {f1}')
-
-------------------------------------------------------------------------------------- #оставить тут MNB
 # Определение функции для оценки модели и вывода F1-меры
-def calculate_f1_score(true, predicted):
+def calculate_f1_score_mnb(true, predicted):
     return f1_score(true, predicted, average='weighted')
 
 # Функция для обучения модели и получения предсказаний
@@ -119,33 +94,7 @@ def mnb_model(model, params, features_train_idf, target_train_encoded, features_
     
     return best_model, predictions
 
---------------------------------------------------------------------------------------# добавить в ноутбук MNB
 
-# Создание сетки параметров
-mnb_grid = {
-    'alpha': [0.1, 0.2, 0.5, 0.8, 1.0],
-    'fit_prior': [True, False],
-    'class_prior': [None]
-}
-
-mnb = MultinomialNB()
-
-# Использование функции для обучения модели
-best_mnb_model, mnb_predictions = mnb_model(mnb, mnb_grid, features_train_idf, target_train, features_test_idf, target_test)
-
-
-
-
-
-
-
-
-
-
-
-
-
-__________________________________________________________________
 def train(model_name, features_train_idf, target_train_encoded, features_test_idf):
     """
     Train your model
